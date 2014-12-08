6aa90c378b3221cab9e41ad95604aa9465bd5db1
Tanjo, Hiroyuki <expheno@gmail.com>
2014-12-08 19:36:53 +0900

    Add Sass compiling and Autoprefixer task

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,22 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
+var assetsDir = './lib/client/assets/';
+
 gulp.task('lint', function () {
-  return gulp.src(['*.js', 'lib/*.js', 'lib/client/assets/scripts/**/*.js'])
+  return gulp.src(['./*.js', './lib/*.js', assetsDir + 'scripts/**/*.js'])
     .pipe($.jscs())
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('styles', function () {
+  return gulp.src(assetsDir + 'styles/*.scss')
+    .pipe($.sass())
+    .pipe($.autoprefixer())
+    .pipe(gulp.dest(assetsDir + 'styles'));
+});
+
 gulp.task('test', ['lint']);
-gulp.task('default', ['test']);
+gulp.task('build', ['styles']);
+gulp.task('default', ['test', 'build']);
