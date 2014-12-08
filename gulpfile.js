22b3d9983884107e83d470992fcd50f86631f5a5
Tanjo, Hiroyuki <expheno@gmail.com>
2014-12-08 17:23:56 +0900

    Tweaks and update directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('lint', function () {
-  return gulp.src(['gulpfile.js', 'lib/*.js', 'lib/assets/scripts/**/*.js'])
+  return gulp.src(['*.js', 'lib/*.js', 'lib/client/assets/scripts/**/*.js'])
     .pipe($.jscs())
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'));
