74ec63a2ebff9216839c03f6b9d82c1e6eec836c
Tanjo, Hiroyuki <expheno@gmail.com>
2014-12-22 12:03:47 +0900

    Include lint task in watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,40 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var pipe = require('multipipe');
 
 var assetsDir = './lib/client/assets';
+var scripts = [
+  './*.js',
+  './lib/*.js',
+  assetsDir + '/scripts/**/*.js'
+];
 
 gulp.task('lint', function () {
-  return gulp.src(['./*.js', './lib/*.js', assetsDir + '/scripts/**/*.js'])
-    .pipe($.jscs())
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'));
+  return pipe(
+    gulp.src(scripts),
+    $.jscs(),
+    $.jshint(),
+    $.jshint.reporter('jshint-stylish'),
+    $.jshint.reporter('fail')
+  );
 });
 
 gulp.task('styles', function () {
   return $.rubySass(assetsDir + '/styles/', {
-      style: 'expanded',
-      sourcemap: true
-    })
-    .on('error', function (err) {
-      console.error('Error!', err.message);
-    })
-    .pipe($.autoprefixer())
-    .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest(assetsDir + '/styles'));
+    style: 'expanded',
+    sourcemap: true
+  })
+  .on('error', function (err) {
+    console.error('Error!', err.message);
+  })
+  .pipe($.autoprefixer())
+  .pipe($.sourcemaps.write('./'))
+  .pipe(gulp.dest(assetsDir + '/styles'));
 });
 
 gulp.task('watch', function () {
+  gulp.watch(scripts, ['lint']);
   gulp.watch(assetsDir + '/styles/*.scss', ['styles']);
 });
 
