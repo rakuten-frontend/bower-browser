8779e30808c34e91e74ffa02e3328773fcf56d15
Tanjo, Hiroyuki <expheno@gmail.com>
2015-01-26 18:12:40 +0900

    Add testing of close event

--- test/server-test.js
+++ test/server-test.js
@@ -3,18 +3,22 @@
 
 var assert = require('assert');
 var http = require('http');
+var _ = require('lodash');
+
 var bowerBrowser = require('../lib/');
+
+var baseOptions = {
+  path: 'test/fixtures',
+  open: false,
+  silent: true
+};
 
 describe('Server', function () {
 
   this.timeout(10000);
 
-  it('should return HTTP response', function (done) {
-    var app = bowerBrowser({
-      path: 'test/fixtures',
-      open: false,
-      silent: true
-    });
+  it('returns HTTP response', function (done) {
+    var app = bowerBrowser(baseOptions);
     app.on('start', function () {
       http.get('http://localhost:3010/', function (res) {
         assert(res.statusCode === 200);
@@ -24,13 +28,27 @@ describe('Server', function () {
     });
   });
 
-  it('should listen specified port', function (done) {
-    var app = bowerBrowser({
-      path: 'test/fixtures',
-      open: false,
-      silent: true,
-      port: 3011
+  it('isn\'t accessible after `close` event', function (done) {
+    var app = bowerBrowser(baseOptions);
+    app.on('start', function () {
+      http.get('http://localhost:3010/', function (res) {
+        assert(res.statusCode === 200);
+        app.close();
+      });
     });
+    app.on('close', function () {
+      http.get('http://localhost:3010/')
+      .on('error', function (error) {
+        assert(error.code = 'ECONNRESET');
+        done();
+      });
+    });
+  });
+
+  it('listens specified port', function (done) {
+    var app = bowerBrowser(_.merge({}, baseOptions, {
+      port: 3011
+    }));
     app.on('start', function () {
       http.get('http://localhost:3011/', function (res) {
         assert(res.statusCode === 200);
