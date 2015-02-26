1b873004c2663f1653aae8b5902a7a56a21c8d38
Tanjo, Hiroyuki <expheno@gmail.com>
2015-02-26 13:42:59 +0900

    Preserve settings in model

--- client/assets/scripts/services/settings.js
+++ client/assets/scripts/services/settings.js
@@ -1,10 +1,37 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = [
   function () {
 
-    var service = {
+    var defaults = {
+      searchFields: {
+        name: true,
+        author: true,
+        description: true
+      },
+      exactMatch: true,
+      ignoreDeprecatedPackages: true,
+      newTab: false,
+      defaultInstallOption: '--save',
+      defaultInstallVersion: ''
     };
+
+    var service = {
+
+      // Active settings
+      data: {},
+
+      // Reset all settings to defaults
+      reset: function () {
+        this.data = _.cloneDeep(defaults);
+      }
+
+    };
+
+    // Initialize setting data
+    service.reset();
 
     return service;
 
