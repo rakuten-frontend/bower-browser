cfc20b015aa54fd02d481c8002def79476d8b435
Tanjo, Hiroyuki <expheno@gmail.com>
2015-02-27 21:44:18 +0900

    Save and load settings on local filesystem

--- client/assets/scripts/controllers/settings.js
+++ client/assets/scripts/controllers/settings.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = [
   '$scope',
   'SettingsService',
@@ -8,6 +10,13 @@ module.exports = [
     // Properties
     $scope.settings = SettingsService;
     $scope.config = SettingsService.config;
+
+    // Save settings when updated
+    $scope.$watch('config', function (newValue, oldValue) {
+      if ($scope.settings.loaded && !_.isEqual(newValue, oldValue)) {
+        $scope.settings.save();
+      }
+    }, true);
 
   }
 ];
--- client/assets/scripts/services/settings.js
+++ client/assets/scripts/services/settings.js
@@ -1,9 +1,15 @@
 'use strict';
 
 var _ = require('lodash');
+_.mixin(require('lodash-deep'));
+
+var settingsApi = '/api/settings.json';
 
 module.exports = [
-  function () {
+  '$http',
+  'SocketService',
+  '$timeout',
+  function ($http, SocketService, $timeout) {
 
     var defaults = {
       searchField: {
@@ -21,11 +27,46 @@ module.exports = [
     var service = {
 
       // Active settings
-      config: {},
+      config: _.cloneDeep(defaults),
+
+      // State for settings
+      loaded: false,
+
+      // Set settings
+      // Invalid properties are ignored
+      set: function (data) {
+        var validData = _.deepMapValues(this.config, function (value, propertyPath) {
+          return _.deepGet(data, propertyPath.join('.'));
+        });
+        _.merge(this.config, validData);
+      },
+
+      // Load settings from server
+      load: function () {
+        var self = this;
+        $http.get(settingsApi)
+          .success(function (data) {
+            self.set(data);
+            $timeout(function () {
+              self.loaded = true;
+            });
+          })
+          .error(function () {
+            self.reset();
+            $timeout(function () {
+              self.loaded = true;
+            });
+          });
+      },
+
+      // Send settings to server
+      save: function () {
+        SocketService.emit('settings', this.config);
+      },
 
       // Reset all settings to defaults
       reset: function () {
-        _.merge(this.config, defaults);
+        this.set(defaults);
       },
 
       // Warn when no search field is selected
@@ -40,7 +81,7 @@ module.exports = [
     };
 
     // Initialize settings
-    service.reset();
+    service.load();
 
     return service;
 
