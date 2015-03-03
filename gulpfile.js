e78c5231807558d8435a7011c0ed0ee02f2fc04e
Tanjo, Hiroyuki <expheno@gmail.com>
2015-03-03 09:56:39 +0900

    Handle error event in Mocha task

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,9 @@ gulp.task('lint', function () {
 gulp.task('mocha', function () {
   return gulp.src(paths.test + '/*-test.js', {read: false})
     .pipe($.mocha({reporter: 'spec'}))
+    .once('error', function () {
+      process.exit(1);
+    })
     .once('end', function () {
       process.exit();
     });
